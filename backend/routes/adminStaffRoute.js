354c2db29b8de0f8519848ca42375f6380efc319
Kanchana Koralage <itsmekanchanakoralage@gmail.com>
2025-03-21 13:53:32 +0530

    backend config,route ,controller,model folders

---
+++ backend/routes/adminStaffRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import {addEmployee,getAllEmployees,updateEmployee,getEmployeeById,deleteEmployee} from "../controller/adminStaffController.js"
+
+const router = express.Router();
+
+// Route to add a new employee
+router.post("/add-employee", addEmployee);
+router.get("/getallemployees", getAllEmployees);
+router.get("/getemployee/:id", getEmployeeById);
+router.put("/updateEmployee/:id", updateEmployee);
+router.delete("/deleteEmployee/:id", deleteEmployee);
+
+export default router;
